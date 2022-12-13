586dc609cd0a1930f7f87a2610e8682a0b6868c6
kaique moreira da silva <kaiquemds@pop-os.localdomain>
2022-12-13 15:31:36 -0300

    add yaml snippet for spring datasource config

--- luasnippets/yaml.lua
+++ luasnippets/yaml.lua
@@ -6,9 +6,4 @@ return {
     t { "", "    username: " }, i(2),
     t { "", "    password: " }, i(3)
   }),
-  s("trig", c(1, {
-    t("Ugh boring, a text node"),
-    i(nil, "At least I can edit something now..."),
-    f(function(args) return "Still only counts as text!!" end, {})
-  }))
 }
