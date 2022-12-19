8e9971eab441577010d2a0eab23d7ba1df3e2b3b
Kaique-MS <kaiquemds@pop-os.localdomain>
2022-12-19 20:06:09 -0300

    remove custom dap config

--- config.lua
+++ config.lua
@@ -1,15 +1,16 @@
 reload "user.options"
 reload "user.plugins"
 reload "user.lsp"
-reload "user.dap"
 reload "user.zen-mode"
 reload "user.keymaps"
 reload "user.lua-line"
 reload "user.telescope"
 reload "user.zk"
 reload "user.autocommands"
+
 require "user.styled".queries()
 require "user.styled".directives()
+
 lvim.builtin.which_key.mappings["t"] = {
   name = "Diagnostics",
   t = { "<cmd>TroubleToggle<cr>", "trouble" },
