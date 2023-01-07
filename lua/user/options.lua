03d78d10bbcf2d2c1213753cf7741a2ab05c9642
Kaique-MS <kaiquemds@pop-os.localdomain>
2023-01-06 21:20:18 -0300

    change themes

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -2,7 +2,7 @@ lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.lsp.diagnostics.virtual_text = true
 lvim.leader = "space"
-lvim.colorscheme = "darkplus"
+lvim.colorscheme = "tokyonight-night"
 
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
@@ -12,5 +12,7 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.breadcrumbs.active = true
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.dap.active = true
-
+-- lvim.builtin.luasnip.sources.friendly_snippets = false
+lvim.builtin.treesitter.rainbow.enable = true
+lvim.builtin.dap.ui.auto_open = false
 
